7f24a64ed47ea619b59f53ee8a96de12e4e0b9ba
sachu29 <sachusaran29.94@gmail.com>
2022-11-02 19:26:25 +0530

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Hello from "./components/HelloWorld.vue";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.component("hello-msg", Hello);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
