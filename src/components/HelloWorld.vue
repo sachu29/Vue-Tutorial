afa26abeea6066be4d7f184f0ffb51eb5a7ef8d9
sachu29 <sachusaran29.94@gmail.com>
2022-11-03 11:01:43 +0530

    Adding two components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>I'm entering Messages here!</h1>
+        <input-box></input-box>
     </div>
 </template>
+
+<script>
+    import Input from './Input-box.vue'
+
+export default {
+    components: {
+        'input-box': Input
+    }
+}
+</script>
---
+++ src/components/Input-box.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input type="text" v-model="message">
+        <p>My Message: {{ message }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data () {
+        return {
+            message: ''
+        }
+    }
+}
+</script>
