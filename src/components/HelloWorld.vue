1d57c118675b8d47a52f327b35490abec7f520bf
sachu29 <sachusaran29.94@gmail.com>
2022-11-03 12:03:22 +0530

    Data binding using Props and own methods

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,21 @@
 <template>
-    <div>
-        <h1>I'm entering Messages here!</h1>
-        <input-box></input-box>
-    </div>
+  <div>
+    <h1>{{ message }}</h1>
+    <input-box :msg="message" @messageChanged="message = $event"></input-box>
+  </div>
 </template>
 
 <script>
-    import Input from './Input-box.vue'
+import Input from "./Input-box.vue";
 
 export default {
-    components: {
-        'input-box': Input
-    }
-}
+  data() {
+    return {
+      message: "I'm entering Messages here!",
+    };
+  },
+  components: {
+    "input-box": Input,
+  },
+};
 </script>
--- src/components/Input-box.vue
+++ src/components/Input-box.vue
@@ -1,17 +1,23 @@
 <template>
-    <div>
-        <input type="text" v-model="message">
-        <p>My Message: {{ message }}</p>
-    </div>
+  <div>
+    <input type="text" :value="msg" @input="changeMessage" />
+    <p>My Message: {{ message }}</p>
+  </div>
 </template>
 
 <script>
-
 export default {
-    data () {
-        return {
-            message: ''
-        }
-    }
-}
+  props: ["msg"],
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    changeMessage(event) {
+      this.message = event.target.value;
+      this.$emit("messageChanged", this.message);
+    },
+  },
+};
 </script>
